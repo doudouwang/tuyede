60d10127c726fb81c1dccd2c00f06210cf0aae6b
tearflyinheaven <ND2sT3GJ2Kp3>
2014-05-27 16:59:20 +0800

    init

---
+++ test/TestGenerator.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import com.abc.task.lang.SerialNumberGenerator;
+
+
+public class TestGenerator {
+	@Test
+	public void test() {
+		System.out.println(SerialNumberGenerator.generate());
+	}
+}
